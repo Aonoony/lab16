151d70f68c4511af917cdd16937e882494983c42
Nitchamon <Aonoony@gmail.com>
2022-01-17 15:50:42 +0700

    "lab16_1.cpp completed"

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,18 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-
-	
-	
+	int a = 5, b ='A', &c = b;
+	int *x = &a, *y = &b, **z = &x;
+	cout << a << ' ' << char(b) << ' ' << char(c) << ' ' << x << ' ' << y << ' ' << z << endl;
+	cout << &a << ' ' << &b << ' ' << &c << ' ' << &x << ' ' << &y << ' ' << &z << endl;
+	c = 'F';
+	cout << a << ' ' << char(b) << ' ' << char(c) << ' ' << x << ' ' << y << ' ' << z << endl;
+	*y = 'W';
+	cout << a << ' ' << char(b) << ' ' << char(c) << ' ' << x << ' ' << y << ' ' << z << endl;
+	*x = 6;
+	cout << a << ' ' << char(b) << ' ' << char(c) << ' ' << x << ' ' << y << ' ' << z << endl;
+	**z = 7;
+	cout << a << ' ' << char(b) << ' ' << char(c) << ' ' << x << ' ' << y << ' ' << z << endl;
 	return 0;
 }
